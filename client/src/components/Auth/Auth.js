b60f0b5617b4e72cf154ed0f49b82fcab56d18bf
Eriola <endoj22@gmail.com>
2021-11-03 13:48:40 +0100

    Auth Component

--- client/src/components/Auth/Auth.js
+++ client/src/components/Auth/Auth.js
@@ -1,12 +1,64 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Avatar, Grid, Container, Button, Paper, Typography, TextField } from '@material-ui/core';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+
+import useStyles from './styles'
+import Input from './Input';
 
 const Auth = () => {
-    const state = null;
+    const classes = useStyles();
+    const [showPassword, setShowPassword] = useState(false);
+    const [isSignUp, setIsSignUp] = useState(false);
+
+    const handleShowPassword = () => setShowPassword((prevShowPassword) => !prevShowPassword );
+
+    const handleSubmit = () => {
+
+    };
+
+    const handleChange = () => {
+
+    };
+
+    const switchMode = () => {
+      setIsSignUp((prevIsSignUp) => !prevIsSignUp);
+      handleShowPassword(false);
+    };
     
     return (
-        <div>
-          AUTH  
-        </div>
+        <Container component = "main" maxWidth = "xs">
+          <Paper className = {classes.paper} elevation = {3}>
+            <Avatar className = {classes.avatar}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography variant = "h5">{ isSignUp ? 'Regjistrohu' : 'Kyçu'}</Typography>
+            <form className = {classes.form} onSubmit = {handleSubmit}>
+              <Grid container spacing = {2}>
+                {
+                  isSignUp && (
+                    <>
+                    <Input name = "firstName" label = "Emri" handleChange = {handleChange} autoFocus half/>
+                    <Input name = "lastName" label = "Mbiemri" handleChange = {handleChange} half/>
+                    </>
+                  )
+                }
+                <Input name = "email" label = "Adresa e emailit" handleChange = {handleChange} type = "email"/>
+                <Input name = "password" label = "Fjalëkalimi" handleChange = {handleChange} type = {showPassword ? "text" : "password"} handleShowPassword = {handleShowPassword}/>
+                { isSignUp && <Input name = "confirmPassword" label = "Rivendosni fjalëkalimin" handleChange = {handleChange} type = "password"/> }
+              </Grid>
+              <Button className = {classes.submit} type = "submit" fullWidth variant = "contained" color = "primary">
+                {isSignUp ? 'Regjistrohu' : 'Kyçu'}
+              </Button>
+              <Grid container justify = "flex-end">
+                <Grid item>
+                  <Button onClick = {switchMode} >
+                    { isSignUp ? 'Keni tashmë një llogari? Identifikohuni' : 'Nuk keni një llogari? Regjistrohuni'}
+                  </Button>
+                </Grid>
+              </Grid>
+            </form>
+          </Paper>
+        </Container>
     )
 }
 
