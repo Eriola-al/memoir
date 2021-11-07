bcaed0cee60ef4547b222d5ab0874ae54e64239a
Eriola <endoj22@gmail.com>
2021-11-07 19:56:57 +0100

    User Actions

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -5,12 +5,14 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import useStyles from './styles';
 import { createPost, updatePost } from '../../actions/posts';
+import { mergeClasses } from '@material-ui/styles';
 
 const Form = ({ currentId, setCurrentId }) => {
-    const [postData, setPostData] = useState({ creator: '', title: '', message: '', tags: '',  selectedFile: '' });
+    const [postData, setPostData] = useState({ title: '', message: '', tags: '',  selectedFile: '' });
     const post = useSelector((state) => currentId ? state.posts.find((p) => p._id === currentId) : null);
     const classes = useStyles();
     const dispatch = useDispatch();
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     useEffect(() => {
         if(post) setPostData(post);
@@ -18,25 +20,35 @@ const Form = ({ currentId, setCurrentId }) => {
 
     const clear = () => {
         setCurrentId(0);
-        setPostData({ creator: '', title: '', message: '', tags: '',  selectedFile: ''});
+        setPostData({ title: '', message: '', tags: '',  selectedFile: ''});
     };
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if(currentId) {
-            dispatch(updatePost(currentId, postData));
+        if(currentId === 0) {
+            dispatch(createPost({...postData, name: user?.result?.name}));
+            clear();
         } else {
-            dispatch(createPost(postData));
+            dispatch(updatePost(currentId, {...postData, name: user?.result?.name}));
         }
         clear();
+    };
+
+    if(!user?.result?.name) {
+        return (
+            <Paper className = {classes.paper}>
+                <Typography variant = "h6" align = "center" >
+                    Ju lutem identifikohu për të krijuar kujtimet tuaja dhe për të pëlqyer kujtimet e të tjerëve.
+                </Typography>
+            </Paper>
+        )
     }
 
     return(
         <Paper className = {classes.paper}>
             <form autoComplete = "off" noValidate className = {` ${classes.root } ${classes.form} `} onSubmit = {handleSubmit}> 
                 <Typography variant = "h6">{currentId ? 'Duke redaktuar' : 'Duke shkruajtur'} kujtimet e mia</Typography>
-                <TextField name="creator" variant="outlined" label="Krijuesi" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })}/>
                 <TextField name="title" variant="outlined" label="Titulli" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })}/>
                 <TextField name="message" variant="outlined" label="Përshkrimi" fullWidth value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })}/>
                 <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })}/>
