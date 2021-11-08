a8dd59348f46f5ee7dbfa3d5b4428c5922d9d802
Eriola <endoj22@gmail.com>
2021-11-08 11:55:52 +0100

    Token Expiry

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import {Link, useHistory, useLocation } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { AppBar, Button, Typography, Toolbar, Avatar } from '@material-ui/core';
+import decode from 'jwt-decode';
 
 import useStyles from './styles';
 import memoir from '../../images/memoir.png';
@@ -25,7 +26,12 @@ const Navbar = () => {
     useEffect(() => {
         const token = user?.token;
 
-        //JWT..
+        if(token){
+            const decodedToken = decode(token);
+
+            if(decodedToken.exp * 1000 < new Date().getTime()) logout();
+        }
+
         setUser(JSON.parse(localStorage.getItem('profile')));
     }, [location]);
 
