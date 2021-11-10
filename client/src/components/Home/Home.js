53e1e369ebbae3bcf0d47a8a926a56eeba7c3bf9
Eriola <endoj22@gmail.com>
2021-11-10 12:43:50 +0100

    Search

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,29 +1,65 @@
 import React, { useState, useEffect} from 'react'
 import { useDispatch} from 'react-redux';
-import { Container, Grow, Grid, Paper } from '@material-ui/core';
+import { Container, Grow, Grid, Paper, AppBar, TextField, Button } from '@material-ui/core';
+import { useHistory, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
 
 import { getPosts } from '../../actions/posts';
 import Pagination from '../Pagination/Pagination';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
-import { mergeClasses } from '@material-ui/styles';
+import useStyles from './styles'
+
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
 
 const Home = () => {
     const [currentId, setCurrentId] = useState(null);
     const dispatch = useDispatch(); 
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const classes = useStyles();
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
 
     useEffect(() => {
         dispatch(getPosts());
-    }, [dispatch]);
+    }, [currentId, dispatch]);
+
+    const searchPost = () => {
+        if(search.trim()){
+            //dispatch -> fetch search post
+        }else{
+            history.push('/');
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if(e.keyCode === 13) {
+            searchPost();
+        }
+    };
+
+    const handleAdd = (tag) => setTags([...tags, tag]);
+
+    const handleDelete = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete));
 
     return (
         <Grow in>
-               <Container>
-                   <Grid container justifyContent="space-between" alignItems = "stretch" spacing = {3}>
-                       <Grid item xs={12} sm={7}> 
+               <Container maxWidth = "xl">
+                   <Grid className = {classes.gridContainer} container justifyContent="space-between" alignItems = "stretch" spacing = {3}>
+                       <Grid item xs={12} sm={6} md={9}> 
                           <Posts setCurrentId = {setCurrentId} />
                        </Grid>
-                       <Grid item xs={12} sm={4}> 
+                       <Grid item xs={12} sm={6} md={3}> 
+                       <AppBar className={classes.appBarSearch} position = "static" color = "inherit">
+                           <TextField name="search" variant = "outlined" label = "Kërko Postime" fullWidth onKeyPress={handleKeyPress} value={search} onChange = {(e) => setSearch(e.target.value)}/>
+                           <ChipInput style={{ margin: '10px 0'}} value={tags} onAdd={handleAdd} onDelete={handleDelete} label="Etiketat e Kërkimit" variant="outlined"/>
+                           <Button variant="contained" onClick={searchPost} color="primary">Kërko</Button>
+                       </AppBar>
                            <Form currentId = {currentId} setCurrentId = {setCurrentId} />
                            <Paper elevation = {6}>
                                <Pagination />
