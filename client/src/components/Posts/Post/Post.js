2c86e35f639c8ade1120fe75182e25c828a5be7c
Eriola <endoj22@gmail.com>
2021-11-15 12:09:40 +0100

    Routing toPost Details

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography } from '@material-ui/core';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
 import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment'; 
 import { useDispatch } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 import useStyles from './styles';
 import { deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const history = useHistory();
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem('profile'));
 
@@ -26,8 +28,12 @@ const Likes = () => {
 }
     return<><ThumbUpAltOutlined fontSize = "small" />&nbsp;Pëlqej</>
 };
+
+const openPost = () => history.push(`/posts/${post._id}`);
+
     return(
         <Card className={classes.card} raised elevation = {6}>
+            <ButtonBase className = { classes.cardAction} onclick = {openPost}>
             <CardMedia className ={classes.media} image = {post.selectedFile} title = {post.title} />
             <div className = {classes.overlay}>
                 <Typography variant="h6">{post.name}</Typography>
@@ -47,6 +53,7 @@ const Likes = () => {
             <CardContent>
                 <Typography variant="body2" color="textSecondary" component="p">{post.message}</Typography>
             </CardContent>
+            </ButtonBase>
             <CardActions className={classes.cardActions}>
                 <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
                     <Likes />
