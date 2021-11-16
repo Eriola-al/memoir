8e7a72869f95658936bd0e85347e8676aeaf28fb
Eriola <endoj22@gmail.com>
2021-11-16 11:08:00 +0100

    Post detailfixes

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -58,7 +58,7 @@ const Form = ({ currentId, setCurrentId }) => {
   return (
     <Paper className={classes.paper} elevation={6}>
       <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        <Typography variant="h6">{currentId ? `Duke reduktuar "${post?.title}"` : 'Postim i Ri'}</Typography>
+        <Typography variant="h6">{currentId ? `Redaktim "${post?.title}"` : 'Postim i Ri'}</Typography>
         <TextField name="title" variant="outlined" label="Titulli" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
         <TextField name="message" variant="outlined" label="Përshkrimi" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
         <div style={{ padding: '5px 0', width: '94%' }}>
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,73 +1,100 @@
-import React from 'react';
-import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core';
+import React, { useState } from 'react';
+import { Card, CardActions, CardContent, CardMedia, Button, Typography, ButtonBase } from '@material-ui/core/';
 import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
-import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
-import moment from 'moment'; 
-import InfoIcon from '@material-ui/icons/Info';
+import ThumbUpAltOutlined from '@material-ui/icons/ThumbUpAltOutlined';
 import { useDispatch } from 'react-redux';
+import moment from 'moment';
 import { useHistory } from 'react-router-dom';
 
+import { likePost, deletePost } from '../../../actions/posts';
 import useStyles from './styles';
-import { getPost, deletePost, likePost } from '../../../actions/posts';
 
 const Post = ({ post, setCurrentId }) => {
-    const classes = useStyles();
-    const history = useHistory();
-    const dispatch = useDispatch();
-    const user = JSON.parse(localStorage.getItem('profile'));
+  const user = JSON.parse(localStorage.getItem('profile'));
+  const [likes, setLikes] = useState(post?.likes);
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const classes = useStyles();
 
-const Likes = () => {
-    if(post?.likes?.length > 0) {
-        return post.likes.find((like) => like === (user?.result?.googleId || user?.result?._id))
+  const userId = user?.result.googleId || user?.result?._id;
+  const hasLikedPost = post?.likes?.find((like) => like === userId);
+
+  const handleLike = async () => {
+    dispatch(likePost(post._id));
+
+    if (hasLikedPost) {
+      setLikes(post.likes.filter((id) => id !== userId));
+    } else {
+      setLikes([...post.likes, userId]);
+    }
+  };
+
+  const Likes = () => {
+    if (likes.length > 0) {
+      return likes.find((like) => like === userId)
         ? (
-            <><ThumbUpAltIcon fontSize = "small" />&nbsp;{post.likes.length > 2 ? `Ti dhe ${post.likes.length - 1} të tjerë` : `${post.likes.length} pëlqim${post.likes.length > 1 ? 'e' : ''}`}</>
+          <><ThumbUpAltIcon fontSize="small" />&nbsp;{likes.length > 2 ? `Ti dhe ${likes.length - 1} të tjerë` : `${likes.length} pëlqim${likes.length > 1 ? 'e' : ''}` }</>
         ) : (
-            <><ThumbUpAltOutlined fontSize = "small" />&nbsp;{post.likes.length} {post.likes.length === 1 ? 'Pëlqim' : 'Pëlqime'} </>
+          <><ThumbUpAltOutlined fontSize="small" />&nbsp;{likes.length} {likes.length === 1 ? 'Pëlqim' : 'Pëlqime'}</>
         );
-}
-    return<><ThumbUpAltOutlined fontSize = "small" />&nbsp;Pëlqej</>;
-};
+    }
 
-const openPost = (e) => history.push(`/posts/${post._id}`);
+    return <><ThumbUpAltOutlined fontSize="small" />&nbsp;Pëlqej</>;
+  };
 
-    return(
-        <Card className={classes.card} raised elevation = {6}>
-            <ButtonBase className = { classes.cardAction} onclick = {openPost} component = "span" name="test">
-            <CardMedia className ={classes.media} image = {post.selectedFile} title = {post.title} />
-            <div className = {classes.overlay}>
-                <Typography variant="h6">{post.name}</Typography>
-                <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
-            </div>
-            {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
-            <div className={classes.overlay2} name="edit">
-                <Button style={{color:'white'}} size = "small" onClick={(e) => {e.stopPropagation(); setCurrentId(post._id); }}>
-                    <MoreHorizIcon fontSize = "default" />
-                </Button>
-            </div>
-            )}
-            <div className={classes.details}>
-            <Typography variant="body2" color="textSecondary" component = "h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
-            </div>
-                <Typography className={classes.title} variant="h5" gutterBottom component="h2">{post.title}</Typography>
-            <CardContent>
-                <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 20).join(' ')}...</Typography>
-            </CardContent>
-            </ButtonBase>
-            <CardActions className={classes.cardActions}>
-                <Button size="small" color="primary" disabled={!user?.result} onClick={() => dispatch(likePost(post._id))}>
-                    <Likes />
-                </Button>
-                {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
-                <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
-                    <DeleteIcon fontSize = "small" /> &nbsp;
-                    Fshi
-                </Button>
-                )}
-            </CardActions>
-        </Card>
-    );
+  const openPost = (e) => {
+    history.push(`/posts/${post._id}`);
+  };
+
+  return (
+    <Card className={classes.card} raised elevation={6}>
+      <ButtonBase
+        component="span"
+        name="test"
+        className={classes.cardAction}
+        onClick={openPost}
+      >
+        <CardMedia className={classes.media} image={post.selectedFile} />
+        <div className={classes.overlay}>
+          <Typography variant="h6">{post.name}</Typography>
+          <Typography variant="body2">{moment(post.createdAt).fromNow()}</Typography>
+        </div>
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+        <div className={classes.overlay2} name="edit">
+          <Button
+            onClick={(e) => {
+              e.stopPropagation();
+              setCurrentId(post._id);
+            }}
+            style={{ color: 'white' }}
+            size="small"
+          >
+            <MoreHorizIcon fontSize="default" />
+          </Button>
+        </div>
+        )}
+        <div className={classes.details}>
+          <Typography variant="body2" color="textSecondary" component="h2">{post.tags.map((tag) => `#${tag} `)}</Typography>
+        </div>
+        <Typography className={classes.title} gutterBottom variant="h5" component="h2">{post.title}</Typography>
+        <CardContent>
+          <Typography variant="body2" color="textSecondary" component="p">{post.message.split(' ').splice(0, 20).join(' ')}...</Typography>
+        </CardContent>
+      </ButtonBase>
+      <CardActions className={classes.cardActions}>
+        <Button size="small" color="primary" disabled={!user?.result} onClick={handleLike}>
+          <Likes />
+        </Button>
+        {(user?.result?.googleId === post?.creator || user?.result?._id === post?.creator) && (
+          <Button size="small" color="secondary" onClick={() => dispatch(deletePost(post._id))}>
+            <DeleteIcon fontSize="small" /> &nbsp; Fshi
+          </Button>
+        )}
+      </CardActions>
+    </Card>
+  );
 };
 
 export default Post;
