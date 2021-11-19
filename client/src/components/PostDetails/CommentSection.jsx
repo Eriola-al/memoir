2d851bf4dd2e8148b174de43356827d80d57c1f2
Eriola <endoj22@gmail.com>
2021-11-19 13:33:16 +0100

    Fetching and Scroll Comments

--- client/src/components/PostDetails/CommentSection.jsx
+++ client/src/components/PostDetails/CommentSection.jsx
@@ -7,14 +7,20 @@ import { commentPost } from '../../actions/posts';
 
 const CommentSection = ({ post }) => {
     const classes = useStyles();
-    const [comments, setComments] = useState([1, 2, 3, 4]);
+    const [comments, setComments] = useState(post?.comments);
     const [comment, setComment] = useState('');
     const dispatch = useDispatch();
     const user = JSON.parse(localStorage.getItem('profile'));
+    const commentsRef = useRef();
 
-    const handleClick = () => {
+    const handleClick = async () => {
         const finalComment = `${user.result.name}: ${comment}`;
-        dispatch(commentPost(finalComment, post._id));
+        const newComments = await dispatch(commentPost(finalComment, post._id));
+
+        setComments(newComments);
+        setComment('');
+
+        commentsRef.current.scrollIntoView({ behavior: 'smooth' });
     };
 
     return(
@@ -24,9 +30,11 @@ const CommentSection = ({ post }) => {
                     <Typography gutterBottom variant = "h6">Komente</Typography>
                     {comments.map((c,i) => (
                         <Typography key={i} gutterBottom variant = "subtitle1">
-                            Komenti {i}
+                           <strong>{c.split(': ')[0]}</strong>
+                           {c.split(':')[1]}
                         </Typography>
                     ))}
+                    <div ref = {commentsRef} />
                 </div>
                 {user?.result?.name && (
                 <div style = {{ width: '70%'}}>
